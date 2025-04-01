33433dd5af373488e907123e5110ce44b6fc5d44
Sean-Staszewski <166570500+Sean-Staszewski@users.noreply.github.com>
2025-04-01 14:49:13 -0400

    Update QPS.h

--- QPS.h
+++ QPS.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <Eigen/Dense>
-#include <cmath>
 
 using namespace Eigen;
 
@@ -8,67 +7,70 @@ class QPSolver {
 
 public:
 
-	struct ModChol {
+    // Struct to perform Modified cholesky check
+    struct ModChol {
 
-		MatrixXd L;
-		MatrixXd D;
-		int flag;
-		QPSolver* QPSolver;
+        int w;
+        MatrixXd L;
+        MatrixXd D;
+        MatrixXd C;
+        bool flag;
+        QPSolver* QPSolver;
 
-		ModChol();
+        // operator for performing cholesky check
+        MatrixXd operator()(MatrixXd& W);
+    };
 
-		MatrixXd operator()(MatrixXd& M);
+    int n; // Dimension of the problem
+    int m; // Number of constraints
 
-	};
+    // Inputs excluding solver_params
+    MatrixXd x; // Current state of the guess, nx1
+    MatrixXd Q; // Quadratic cost (1/2x'*Q*x), nxn
+    MatrixXd F; // Linear cost (f'*x), nx1
+    MatrixXd A; // Matrix associated with linear cost (Ax<=b) mxn
+    MatrixXd B; // Vector associated woth linear cost (Ax<=b) mx1
 
-	//Inputs:
-	int n;
-	int m;
+    // Variables that will be determined
+    MatrixXd lambda; // Lagrange multiplier
+    MatrixXd tslack; // Slack variables
+    MatrixXd dx; // x's direction for step
+    MatrixXd dlam; // lambda's direction for step
+    MatrixXd dtsl; // tslack's direction for step
+    float mu; // complimentary gap
+    MatrixXd fval;
+    int iter;
 
-	MatrixXd x;
-	MatrixXd Q;
-	MatrixXd F;
-	MatrixXd A;
-	MatrixXd B;
+    //Tuneable variables
+    int maxiter;	//between 5 and 1000
+    float tol;		//between 1e-12 and 1e0
+    float sigma;	//Convergence variable
 
-	//Determined vars
-	MatrixXd lambda;
-	MatrixXd tslack;
-	MatrixXd dx;
-	MatrixXd dlam;
-	MatrixXd dtsl;
+    //Random params
+    MatrixXd alg_diag;
+    float zero_tol;
+    float delta_l;
+    float beta_l;
+    float delta_u;
+    float beta_u;
+    float alpha_dec;
 
-	float mu;
-	float fval;
-	int iter;
+    // QPSolver's struct for modified cholesky check
+    ModChol chol;
 
-	//Tuneable vars
-	int maxiter;	//between 5 and 1000
-	float tol;		//between 1e-12 and 1e0
-	float sigma;	//Convergence var
+    // Class Definition
+    QPSolver(MatrixXd& x_, MatrixXd& Q, MatrixXd& F, MatrixXd& A_, MatrixXd& B_, float solver_param[]);
 
-	//Random params
-	MatrixXd alg_diag;
-	float zero_tol;
-	float delta_l;
-	float beta_l;
-	float delta_u;
-	float beta_u;
-	float alpha_dec;
+    // Resets all determined and input variables
+    void Reset(MatrixXd& x_, MatrixXd& Q_, MatrixXd& F_, MatrixXd& A_, MatrixXd& B_);
 
-	ModChol chol;
+    // 0 means success, 1 means numerical issue, 2 means invalid input
+    int ModMat(MatrixXd& M);
 
-	QPSolver();
+    //update's the optimal direction for all step direction variables
+    void StepDir();
 
-	QPSolver(MatrixXd& x_, MatrixXd& Q, MatrixXd& F, MatrixXd& A_, MatrixXd& B_, float& solver_param);
+    // Main functionality method of QPSolver, iteratively
+    void Solve();
 
-	~QPSolver();
-
-	void Reset();
-
-	void StepDir();
-
-	MatrixXd ModMat(MatrixXd& M);
-
-	void Solve();
 };
